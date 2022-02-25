b9eac7756f79ab1c8c5b54ba35adbff3e582d091
Pedro Selvate <pedroselvate72@gmail.com>
2022-02-25 10:42:02 -0300

    Configurações de segurança

--- frontend/src/views/Register/Register.vue
+++ frontend/src/views/Register/Register.vue
@@ -3,18 +3,24 @@
     <img class="bg-effect" src="@/assets/fundo.png">
     <div class="login">
       <img class="logo" src="@/assets/logo_gtchef.png" alt="Logo da GTchef">
-      <form >
+      <form @submit.prevent="handleSubmit">
          
+        <label>Primeiro nome</label>
+         <input type="text" v-model="primeiro_nome">
+
+         <label>Último nome</label>
+         <input type="text" v-model="ultimo_nome">
+
          <label>E-mail</label>
-         <input type="email">
+         <input type="email" v-model="email">
 
          <label>Senha</label>
-         <input type="password">
+         <input type="password" v-model="senha">
 
          <label>Confirmar senha</label>
-         <input type="password">
+         <input type="password" v-model="confirmar_senha">
 
-        <button type="submit">Cadastrar</button>
+        <button>Cadastrar</button>
         <a href="#" class="cancel-btn">Cancelar</a>
       </form>
 
@@ -23,8 +29,39 @@
 </template>
 
 <script>
-export default {
 
+import axios from "axios";
+
+export default {
+  name: 'Register',
+
+  data(){
+    return{
+      primeiro_nome: '',
+      ultimo_nome: '',
+      email: '',
+      senha: '',
+      confirmar_senha: '',
+    }
+  },
+
+  methods: {
+    handleSubmit() {
+
+      const URL_API = "http://localhost:8080/api/v1/new-user";
+
+      const data = {
+        primeiro_nome: this.primeiro_nome,
+        ultimo_nome: this.ultimo_nome,
+        email: this.email,
+        senha: this.senha,
+        confirmar_senha: this.confirmar_senha,
+      }
+
+      axios.post(URL_API, data)
+
+    }
+  }
 }
 </script>
 
